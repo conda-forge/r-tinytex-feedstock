a7e6832c46e90e4990be0f6f3941b955ab15d4d7
Mervin Fansler <fanslerm@mskcc.org>
2023-10-24 16:48:17 -0400

    emit note on perl requirement
    discussion in https://github.com/rstudio/tinytex/issues/419

---
+++ recipe/post-link.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo " " >> $PREFIX/.messages.txt
+echo "To install TinyTeX with `tinytex::install_tinytex()` the system must have a functional Perl" >> $PREFIX/.messages.txt
+echo "installation with a `File::Find` module. Most end-user systems will already satisfy this" >> $PREFIX/.messages.txt
+echo "requirement; however, some minimal contexts (e.g., containers) may not. Perl is available" >> $PREFIX/.messages.txt
+echo "via Conda Forge as the package `perl`. See https://github.com/rstudio/tinytex/issues/419." >> $PREFIX/.messages.txt
+echo " " >> $PREFIX/.messages.txt
